5e5deea42c5032e840d6765e376cefb791d3448e
Guchu <guchu413@gmail.com>
2022-07-16 21:09:49 +0300

    add function to event listener

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ sheds = ['A','B','C','D']
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 thirtyDays = ['September','April','June', 'November']
 feb = ['February']
+
 const shedOutput = {
     A : Math.ceil((Math.random()* 700)+200),
     B : Math.ceil((Math.random()*700)+200),
@@ -14,11 +15,9 @@ const shedOutput = {
     D : Math.ceil((Math.random()* 700)+200),
 }
 
-const totalOutput = Math.ceil(shedOutput.A +
-     shedOutput.B + shedOutput.C + shedOutput.D);
-
+const totalOutput = Math.ceil(shedOutput.A +shedOutput.B + shedOutput.C + shedOutput.D);
 const totalPrice = totalOutput * milkPrice;
-console.log(totalOutput+':'+ totalPrice);
+
 
 
 
