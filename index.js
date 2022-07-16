50ad2b8afcb76d5f53b43b1373768b4ea1b02625
Guchu <guchu413@gmail.com>
2022-07-16 11:27:47 +0300

    add buttons

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ totalProduction = function(sheds, shedOutput) {
     for(i=0; i<sheds.length; i++) {
     output.push(`<li>Your production in Shed ${sheds[i]} : ${Object.values(shedOutput)[i]} litres per day</li>`)
     }
-    output.push(`<li>The total output is ${totalOutput} litres for the week</li>`)
+    output.push(`<li>The total output is ${totalOutput} litres for the day</li>`)
+    output.push(`<li>The total output is ${totalOutput*7} litres for the week</li>`)
     dayOutput = totalOutput
     return(output)
     //console.log(`The total income for the day is ${totalOutput*milkPrice}`)
@@ -42,43 +43,34 @@ totalProduction = function(sheds, shedOutput) {
 // //////////////////////////////////////////////////////
 
 
-function incomeOverTime(sellingPrice, time, input = dayOutput){
+function incomeOverTime(sellingPrice, input = totalOutput){
     let price = sellingPrice;
     let profit;
     let income = price * input;
-    if (time=="weekly"){
-        profit = income*7;
-        console.log (`Your income for ${time} is ${profit}`)
+    let month
+    let monthIncome = []
+    for (i=0; i<12; i++){
+        month = months[i];
+        if (thirtyDays.includes(month)){
+            profit = income*30;
+            month =  (`<li>Your income for ${months[i]} is ${profit}</li>`)
+        }
+        
+        else if (month == "February") {
+            profit = income*29;
+            month = (`<li>Your income for ${months[i]} is ${profit}</li>`)
+        }
+ 
+        else{
+            profit = income*31;
+            month =  (`<li>Your income for ${months[i]} is ${profit}</li>`)
+        }
+        monthIncome.push(month)
     }
+    return monthIncome   
 
-    else if (time=="monthly"){
-        for (i=0; i<12; i++){
-            month = months[i];
-            if (thirtyDays.includes(month)){
-                profit = income*30;
-                console.log (`Your income for ${months[i]} is ${profit}`)
-            }
-            
-            else if (month == "February") {
-                profit = income*29;
-                console.log (`Your income for ${months[i]} is ${profit}`)
-            }
-
-            else{
-                profit = income*31;
-                console.log (`Your income for ${months[i]} is ${profit}`)
-            }
-    }   
-    }
-    else if (time=="year"){
-        profit = income*366
-        console.log(`Your income for the whole year is ${profit}`)
-    }
-    
-    else{
-        console.log ("Enter a valid timeframe")
-    }
 }
+
 
 
 
@@ -101,11 +93,30 @@ button1.addEventListener('click', () => {
     content1.innerHTML = `${prod}`
 })
 
-const button2 = document.getElementById('buttonB');
+const button2 = document.querySelector('.buttonB');
 button2.addEventListener('click', () =>{
-    sellingPrice = prompt("Enter the selling price for a liter");
-    let weekly = sellingPrice * dayOutput * 7;
+    let weekly,yearly
+    if(!(sellingPrice)){
+        sellingPrice = prompt("Enter the selling price for a liter");
+        weekly = sellingPrice * totalOutput * 7;
+        yearly = sellingPrice * totalOutput * 366;}
+    else {
+         weekly = sellingPrice * totalOutput * 7;
+         yearly = sellingPrice * totalOutput * 366;}
     let content2 = document.getElementById('button2');
-    content2.innerHTML = `for the week: ${weekly}`
+    content2.innerHTML = `Your weekly income will be : Ksh. ${weekly}<br> Your yearly income will be : Ksh. ${yearly}`
+
+})
+
+const button3 = document.querySelector('.buttonC')
+button3.addEventListener('click', () => {
+    if (sellingPrice){
+    monthly = incomeOverTime(sellingPrice)}
+    else{
+        sellingPrice = prompt('Enter the desired price for a liter.')
+        monthly = incomeOverTime(sellingPrice)
+    }
+    let button3 = document.getElementById('button3')
+    button3.innerHTML =  `${monthly}`
 
 })
